3f775974a91dd6ff7893699e558832aa4797dca7
BosEriko <bos.eriko@gmail.com>
2024-06-28 13:47:37 +0800

    add alias and config zsh

---
+++ alias.z.sh
@@ -0,0 +1,10 @@
+
+# ================================================================================== [Alias] ===== #
+
+alias open="explorer.exe"
+alias copy="clip.exe"
+alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
+alias sudo="echo $PASSWORD | sudo -S "
+
+alias start="start-session"
+alias end="end-session"
---
+++ config.z.sh
@@ -0,0 +1,23 @@
+
+# ================================================================================= [Config] ===== #
+
+# Path to your oh-my-zsh installation
+export ZSH=$HOME/.oh-my-zsh
+
+# History Configuration
+HISTFILE="~/.zsh_history"
+HISTSIZE=10000
+SAVEHIST=10000
+
+# Disable Compfix
+ZSH_DISABLE_COMPFIX=true
+
+# Source ZSH
+source $ZSH/oh-my-zsh.sh
+
+# Set Theme
+ZSH_THEME=robbyrussell
+
+# Use nano as the editor
+export VISUAL=nano
+export EDITOR=nano
