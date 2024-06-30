f3d9ff879cdbab2c3103108de24375168b12361c
BosEriko <bos.eriko@gmail.com>
2024-06-30 18:52:21 +0800

    add alias for code

--- alias.z.sh
+++ alias.z.sh
@@ -6,11 +6,12 @@ alias copy="clip.exe"
 alias npm="echo 'Do you really want to use NPM instead of Yarn? (Ctrl-C to abort, or press enter to continue)' && read && npm"
 alias sudo="echo $PASSWORD | sudo -S "
 
-alias nvim="code -r ."
-alias vim="code -r ."
-alias vi="code -r ."
-alias vs="code -r ."
-alias v="code -r ."
+code-alias() { [ $# -eq 0 ] && code -r . || code -r "$@"; }
+alias nvim="code-alias"
+alias vim="code-alias"
+alias vi="code-alias"
+alias vs="code-alias"
+alias v="code-alias"
 
 alias start="start-session"
 alias end="end-session"
