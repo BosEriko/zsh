ce71264230b51cfd1fa44295eafbdafba32cc7cd
BosEriko <bos.eriko@gmail.com>
2024-07-06 00:22:45 +0800

    combine start and end session file

--- session.z..sh
+++ session.z..sh
@@ -1,7 +1,15 @@
 
-# ============================================================================ [End Session] ===== #
+# ========================================================================== [Start Session] ===== #
 
-# To Use: auto-push <FOLDER_PATH> <FOLDER_NAME>
+auto-pull() {
+  (
+    cd $1
+    git checkout .
+    git checkout master && git pull origin master
+    echo "${GREEN}${2} Repository has been synced successfully.${RESET}"
+  )
+}
+
 auto-push() {
   (
     cd $1
@@ -16,6 +24,21 @@ auto-push() {
     fi
   )
 }
+
+start-session() {
+  # configuration
+  auto-pull ~ "Configuration"
+  # Source ZSH
+  printf "${B_YELLOW}Do you want to source your ZSH config (y/n)? ${RESET}"
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ] ;then
+    source ~/.zshrc
+    echo "${GREEN}${2}ZSH has been sourced successfully.${RESET}"
+  else
+    echo "${YELLOW}${2}ZSH has not been sourced.${RESET}"
+  fi
+}
+
 
 end-session() {
   # history
