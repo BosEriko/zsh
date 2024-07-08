331c598967e6d63339079f0b8636aaeb58d4cfa4
BosEriko <bos.eriko@gmail.com>
2024-07-08 16:13:10 +0800

    fix vscode directory

---
+++ fix.z.sh
@@ -0,0 +1,5 @@
+
+# =============================================================================== [Fix] ===== #
+
+# This is a Visual Studio Code fix to avoid errors when not in root directory on terminal open
+[[ "$PWD" == "$WINDOWS_USER_DIRECTORY" ]] && cd ~
