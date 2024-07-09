88f664035d6c3fb19741c34a966dbb91e64491c1
BosEriko <bos.eriko@gmail.com>
2024-07-09 09:32:12 +0800

    rename fix to hotfix

--- hotfix.z.sh
+++ hotfix.z.sh
@@ -1,5 +1,5 @@
 
-# =============================================================================== [Fix] ===== #
+# ============================================================================ [Hotfix] ===== #
 
 # This is a Visual Studio Code fix to avoid errors when not in root directory on terminal open
 [[ "$PWD" == "$WINDOWS_USER_DIRECTORY" ]] && cd ~
