aac43151b108d30cabbbf7a12c85457000c30bc8
BosEriko <bos.eriko@gmail.com>
2024-07-12 10:46:28 +0800

    update fnm

--- source.z.sh
+++ source.z.sh
@@ -5,7 +5,7 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # Source fnm
-export PATH=/home/boseriko/.fnm:$PATH && eval "$(fnm env --use-on-cd)"
+export PATH=$HOME/.fnm:$PATH && eval "$(fnm env --use-on-cd)"
 
 # Source rbenv
 export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
