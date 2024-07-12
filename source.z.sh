756dd92f19031c73e9c0c1f6a24ea1ca1ce330a4
BosEriko <bos.eriko@gmail.com>
2024-07-12 11:36:21 +0800

    update fnm sourcing

--- source.z.sh
+++ source.z.sh
@@ -5,7 +5,11 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # Source fnm
-export PATH=$HOME/.fnm:$PATH && eval "$(fnm env --use-on-cd)"
+FNM_PATH="$HOME/.local/share/fnm"
+if [ -d "$FNPM_PATH" ]; then
+    export PATH="$FNM_PATH:$PATH"
+    eval "`fnm env`"
+fi
 
 # Source rbenv
 export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
