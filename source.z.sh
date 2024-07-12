94e27cf1bd2d741e799fe3d1e5d9e7158885507b
BosEriko <bos.eriko@gmail.com>
2024-07-12 14:42:47 +0800

    fix fnm and ruby path

--- source.z.sh
+++ source.z.sh
@@ -5,11 +5,12 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # Source fnm
-FNM_PATH="$HOME/.local/share/fnm"
+FNM_PATH="$HOME/.fnm"
 if [ -d "$FNPM_PATH" ]; then
     export PATH="$FNM_PATH:$PATH"
-    eval "`fnm env`"
+    eval "$(fnm env --use-on-cd)"
 fi
 
 # Source rbenv
 export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
