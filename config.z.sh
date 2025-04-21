2bdc6e9914316b939285bdf95824b2a0f024e65d
BosEriko <bos@eriko.ph>
2025-04-21 20:08:34 +0800

    fix basename issue for prompt

--- config.z.sh
+++ config.z.sh
@@ -16,7 +16,7 @@ ZSH_DISABLE_COMPFIX=true
 source $ZSH/oh-my-zsh.sh
 
 # Remove Line Name
-PROMPT='%F{blue}$(basename $(git rev-parse --show-toplevel 2>/dev/null) || echo %~)%f $(git rev-parse --abbrev-ref HEAD 2>/dev/null) %F{yellow}%(!.#.>)%f '
+PROMPT='%F{blue}$(basename "$(git rev-parse --show-toplevel 2>/dev/null)" 2>/dev/null || echo %~)%f $(git rev-parse --abbrev-ref HEAD 2>/dev/null) %F{yellow}%(!.#.>)%f '
 
 # Use nano as the editor
 export VISUAL=nano
