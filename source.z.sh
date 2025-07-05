ee93949483ccbf30b178a7248ce7d168e5df63d6
BosEriko <bos@eriko.ph>
2025-07-05 17:32:33 +0800

    Feature: Combine src and Source

--- source.z.sh
+++ source.z.sh
@@ -19,3 +19,6 @@ export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 installs_dir=$HOME/.elixir-install/installs
 export PATH=$installs_dir/otp/27.1.2/bin:$PATH
 export PATH=$installs_dir/elixir/1.18.2-otp-27/bin:$PATH
+
+# Source ./local/bin
+export PATH="$HOME/.local/bin:$PATH"
