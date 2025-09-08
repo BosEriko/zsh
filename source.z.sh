0cbcca36ac2948ed8a509c589aaf1d1d931a99fb
BosEriko <bos@eriko.ph>
2025-09-08 13:42:22 +0800

    Feature: Remove Elixir Installers

--- source.z.sh
+++ source.z.sh
@@ -15,10 +15,5 @@ fi
 export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
 
-# Source elixir
-installs_dir=$HOME/.elixir-install/installs
-export PATH=$installs_dir/otp/27.1.2/bin:$PATH
-export PATH=$installs_dir/elixir/1.18.2-otp-27/bin:$PATH
-
 # Source ./local/bin
 export PATH="$HOME/.local/bin:$PATH"
