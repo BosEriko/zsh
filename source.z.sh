df3626404e4e844d1dc045ca470ccce814d026fd
BosEriko <bos@eriko.ph>
2025-09-10 14:45:25 +0800

    Feature: Source Yarn

--- source.z.sh
+++ source.z.sh
@@ -11,6 +11,9 @@ if [ -d "$FNM_PATH" ]; then
     eval "$(fnm env --use-on-cd)"
 fi
 
+# Source Yarn
+export PATH="$HOME/.yarn/bin:$PATH"
+
 # Source rbenv
 export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
