b1439d3efa9d0178e4bd46b1f41ca59029f1c3cb
BosEriko <bos@eriko.ph>
2026-01-26 09:32:53 +0800

    revert ubuntu config

---
+++ config/ubuntu.z.sh
@@ -0,0 +1,23 @@
+
+# ================================================================================= [Config] ===== #
+
+# Path to your oh-my-zsh installation
+export ZSH=$HOME/.oh-my-zsh
+
+# History Configuration
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=10000
+SAVEHIST=10000
+
+# Disable Compfix
+ZSH_DISABLE_COMPFIX=true
+
+# Source Oh My ZSH
+source $ZSH/oh-my-zsh.sh
+
+# Remove Line Name
+PROMPT='%F{blue}$(basename "$(git rev-parse --show-toplevel 2>/dev/null)" 2>/dev/null || echo %~)%f $(git rev-parse --abbrev-ref HEAD 2>/dev/null) %F{yellow}%(!.#.>)%f '
+
+# Use nano as the editor
+export VISUAL=nano
+export EDITOR=nano
