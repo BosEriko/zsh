e8156c52e06475c8154d10d496aa87856943646a
BosEriko <bos@eriko.ph>
2026-02-03 21:14:35 +0800

    Feature: Prioritize Warning on Notes Checker

--- notes/mac.z.sh
+++ notes/mac.z.sh
@@ -9,10 +9,10 @@ save-notes() {
   local YESTERDAY="$(date -v-1d +%Y-%m-%d)"
   local DAY_OF_WEEK="$(date +%u)"
 
-  if [[ "$DAY_OF_WEEK" -eq 6 || "$DAY_OF_WEEK" -eq 7 || ( -f "$STATE_FILE" && "$(cat "$STATE_FILE")" == "$TODAY" ) ]]; then
-    return
-  elif [[ -f "$WARNING_FILE" ]]; then
+  if [[ -f "$WARNING_FILE" ]]; then
     clear && echo "${YELLOW}${2}Changes on the cloud is still not pulled. Please fix manually.${RESET}"
+    return
+  elif [[ "$DAY_OF_WEEK" -eq 6 || "$DAY_OF_WEEK" -eq 7 || ( -f "$STATE_FILE" && "$(cat "$STATE_FILE")" == "$TODAY" ) ]]; then
     return
   fi
 
@@ -21,15 +21,15 @@ save-notes() {
 
     git fetch origin master >/dev/null 2>&1
     if git rev-list --count HEAD..origin/master | grep -qv '^0$'; then
-      echo "$TODAY" > "$STATE_FILE"
       echo "1" > "$WARNING_FILE"
       clear && echo "${YELLOW}${2}Changes on the cloud detected. Skipping sync today. Please fix manually.${RESET}"
       return
+    else
+      rm -f "$WARNING_FILE"
     fi
 
     echo "${CYAN}${2} Syncing Notes.${RESET}"
     echo "$TODAY" > "$STATE_FILE"
-    rm -f "$WARNING_FILE"
     git add .
     git commit -m ":pencil: $YESTERDAY"
     git push origin master
