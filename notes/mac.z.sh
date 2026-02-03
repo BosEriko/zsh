7d1791311918b1d1d7bb9ad051fb9473a4709d57
BosEriko <bos@eriko.ph>
2026-02-03 21:23:47 +0800

    Feature: Remove Warning File if there are no more changes

--- notes/mac.z.sh
+++ notes/mac.z.sh
@@ -10,9 +10,13 @@ save-notes() {
   local DAY_OF_WEEK="$(date +%u)"
 
   if [[ -f "$WARNING_FILE" ]]; then
+    if [[ $(git rev-list --count HEAD..origin/master) -eq 0 ]]; then
+      rm -f "$WARNING_FILE"
+      return
+    fi
     clear && echo "${YELLOW}${2}Changes on the cloud is still not pulled. Please fix manually.${RESET}"
     return
-  elif [[ "$DAY_OF_WEEK" -eq 6 || "$DAY_OF_WEEK" -eq 7 || ( -f "$STATE_FILE" && "$(cat "$STATE_FILE")" == "$TODAY" ) ]]; then
+  elif [[ "$DAY_OF_WEEK" -ge 6 || ( -f "$STATE_FILE" && "$(cat "$STATE_FILE")" == "$TODAY" ) ]]; then
     return
   fi
 
@@ -20,21 +24,19 @@ save-notes() {
     cd "$NOTES_DIR" || return
 
     git fetch origin master >/dev/null 2>&1
-    if git rev-list --count HEAD..origin/master | grep -qv '^0$'; then
+    if [[ $(git rev-list --count HEAD..origin/master) -ne 0 ]]; then
       echo "1" > "$WARNING_FILE"
       clear && echo "${YELLOW}${2}Changes on the cloud detected. Skipping sync today. Please fix manually.${RESET}"
       return
-    else
-      rm -f "$WARNING_FILE"
     fi
 
     echo "${CYAN}${2} Syncing Notes.${RESET}"
     echo "$TODAY" > "$STATE_FILE"
+    rm -f "$WARNING_FILE"
     git add .
     git commit -m ":pencil: $YESTERDAY"
     git push origin master
     clear && echo "${GREEN}${2}Notes has been synced.${RESET}"
   )
-
 }
 save-notes
