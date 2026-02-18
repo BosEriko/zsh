637c7980a2d5d0fffebbf3004b16c3dda8656ca1
BosEriko <bos@eriko.ph>
2026-02-18 17:15:10 +0800

    Feature: Revert Tmux to what I have before

---
+++ .files/tmux/plugins.tmux.sh
@@ -0,0 +1,10 @@
+# ================================================================================ [Plugins] ===== #
+
+# TPM
+set -g @plugin 'tmux-plugins/tpm'
+
+# Theme
+set -g @plugin 'catppuccin/tmux#v2.1.3'
+
+# Initialize the plugin manager
+run '~/.tmux/plugins/tpm/tpm'
