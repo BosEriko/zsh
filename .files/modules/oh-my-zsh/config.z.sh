c2688a6f1b964b308ac006769a6590ed674c3838
BosEriko <bos@eriko.ph>
2026-02-22 17:58:06 +0800

    Feature: Add Antigen Plugins

--- .files/modules/oh-my-zsh/config.z.sh
+++ .files/modules/oh-my-zsh/config.z.sh
@@ -3,4 +3,13 @@
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
 
+source $HOME/.nix-profile/share/antigen/antigen.zsh
+export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Has alias: "
+antigen use oh-my-zsh
+antigen bundle djui/alias-tips
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen apply
+
 source $ZSH/oh-my-zsh.sh
