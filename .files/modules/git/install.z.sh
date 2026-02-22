00a741035214de1c6f898588c115c858e67dddac
BosEriko <bos@eriko.ph>
2026-02-22 17:31:26 +0800

    Feature: Add GIT

---
+++ .files/modules/git/install.z.sh
@@ -0,0 +1,5 @@
+# =========================================================================== [Installation] ===== #
+
+if [[ "$OS_TYPE" == "stm" ]]; then
+  echo "[Modules — Git] Installed by Default"
+fi
