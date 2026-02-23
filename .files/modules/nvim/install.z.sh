4f52800a33cea183492070e94eeede0ed332756d
BosEriko <bos@eriko.ph>
2026-02-23 22:14:34 +0800

    Feature: Add LazyVim to neovim installer

--- .files/modules/nvim/install.z.sh
+++ .files/modules/nvim/install.z.sh
@@ -2,5 +2,7 @@
 
 # Install NeoVim
 if [[ "$OS_TYPE" == "stm" ]]; then
-  nix-env -iA nixpkgs.neovim
+  nix-env -iA nixpkgs.neovim nixpkgs.gcc
+  rm -rf ~/.config/nvim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
 fi
