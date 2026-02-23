4b8a660a59047fa2e106ba4861e49903742f10cc
BosEriko <bos@eriko.ph>
2026-02-23 22:30:33 +0800

    Feature: Only run neofetch on the very first tmux opening

--- .files/initializers/steamos/config.z.sh
+++ .files/initializers/steamos/config.z.sh
@@ -1,7 +1,8 @@
 # ========================================================================== [Configuration] ===== #
 
 if [[ "$OS_TYPE" == "stm" ]]; then
-  if [ ! "$TMUX" = "" ]; then
+  if [ ! "$TMUX" = "" ] && [ -z "$TMUX_FIRST_RUN" ]; then
     neofetch
+    export TMUX_FIRST_RUN=1
   fi
 fi
