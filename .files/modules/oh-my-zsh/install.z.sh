3a309108334b840e774785527321a985a95b6d38
BosEriko <bos@eriko.ph>
2026-02-23 12:15:17 +0800

    Feature: Move antigen install on install script

--- .files/modules/oh-my-zsh/install.z.sh
+++ .files/modules/oh-my-zsh/install.z.sh
@@ -1,3 +1,9 @@
 # =========================================================================== [Installation] ===== #
 
+# Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Antigen
+if [[ "$OS_TYPE" == "stm" ]]; then
+  nix-env -iA nixpkgs.antigen
+fi
