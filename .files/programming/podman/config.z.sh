e501de62d53dec4a8e803bd115ffcf8815022eb6
BosEriko <bos@eriko.ph>
2026-02-24 19:30:45 +0800

    Feature: Alias podman to docker

--- .files/programming/podman/config.z.sh
+++ .files/programming/podman/config.z.sh
@@ -1,3 +1,6 @@
 # ========================================================================== [Configuration] ===== #
 
+alias docker="podman"
+alias docker-compose="podman-compose"
+
 bos-append manual podman "Print Podman Manual" "podman --help"
