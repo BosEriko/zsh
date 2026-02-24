a8571603e50f6352ca7cccefcc5c18f9d7cf8e83
BosEriko <bos@eriko.ph>
2026-02-24 16:01:19 +0800

    Feature: Delete Yazi

--- .files/modules/explorer/install.z.sh
+++
@@ -1,7 +0,0 @@
-# =========================================================================== [Installation] ===== #
-
-# Install yazi dependencies
-if [[ "$OS_TYPE" == "stm" ]]; then
-  nix-env -iA nixpkgs.yazi
-  nix-env -iA nixpkgs.ffmpeg
-fi
