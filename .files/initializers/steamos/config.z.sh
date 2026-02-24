821d0013c9d56feef5703d52a230eaab16e98cbc
BosEriko <bos@eriko.ph>
2026-02-24 10:20:16 +0800

    Feature: Add Instruction to list all commands

--- .files/initializers/steamos/config.z.sh
+++ .files/initializers/steamos/config.z.sh
@@ -4,6 +4,7 @@ if [[ "$OS_TYPE" == "stm" ]]; then
   if [ ! "$TMUX" = "" ]; then
     if ! tmux show-environment -g NEOFETCH_DONE >/dev/null 2>&1; then
       neofetch
+      echo "Run 'bos -h' to list all custom commands."
       tmux set-environment -g NEOFETCH_DONE 1
     fi
   fi
