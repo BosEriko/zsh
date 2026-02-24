507c2c4ce0cf58e4b5cfec4daeaa12e6ace0bc8b
BosEriko <bos@eriko.ph>
2026-02-24 16:15:28 +0800

    Feature: Add Ruby Programming

---
+++ .files/programming/ruby/config.z.sh
@@ -0,0 +1,4 @@
+# ========================================================================== [Configuration] ===== #
+
+# Source rbenv
+export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
