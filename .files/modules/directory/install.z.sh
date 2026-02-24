c04f494f5ec1a0c398bd3595ecd8e441e78b56a6
BosEriko <bos@eriko.ph>
2026-02-24 15:26:50 +0800

    Feature: Create Directory Module

---
+++ .files/modules/directory/install.z.sh
@@ -0,0 +1,7 @@
+# =========================================================================== [Installation] ===== #
+
+# Make Directories
+mkdir -p ~/Documents/Codes/Work
+mkdir -p ~/Documents/Codes/Personal
+mkdir -p ~/Documents/Codes/Configuration
+mkdir -p ~/Documents/Notes
