c8584c0c11404ea76d580fc448b2dcd233973c12
BosEriko <bos@eriko.ph>
2026-02-24 12:50:10 +0800

    Feature: Directly add fzf to configuration file

--- .files/modules/fzf/config.z.sh
+++ .files/modules/fzf/config.z.sh
@@ -1,3 +1,7 @@
 # ========================================================================== [Configuration] ===== #
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [[ ! "$PATH" == */home/deck/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/deck/.fzf/bin"
+fi
+
+source <(fzf --zsh)
