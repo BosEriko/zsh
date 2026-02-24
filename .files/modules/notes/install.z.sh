e322d914711ba290b73015f43477e499040d9c5e
BosEriko <bos@eriko.ph>
2026-02-24 11:56:01 +0800

    Feature: Scope git initialization of notes

--- .files/modules/notes/install.z.sh
+++ .files/modules/notes/install.z.sh
@@ -4,13 +4,15 @@
 if [[ "$OS_TYPE" == "stm" ]]; then
   NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.obsidian
   mkdir -p ~/Documents/Notes
-  cd ~/Documents/Notes
-  git init
-  git remote add origin git@github.com:BosEriko/obsidian.git
-  git remote add github git@github.com:BosEriko/obsidian.git
-  git remote add gitlab git@gitlab.com:BosEriko/obsidian.git
-  git remote add bitbucket git@bitbucket.org:BosEriko/obsidian.git
-  git remote set-url --add --push origin git@github.com:BosEriko/obsidian.git
-  git remote set-url --add --push origin git@gitlab.com:BosEriko/obsidian.git
-  git remote set-url --add --push origin git@bitbucket.org:BosEriko/obsidian.git
+  (
+    cd ~/Documents/Notes
+    git init
+    git remote add origin git@github.com:BosEriko/obsidian.git
+    git remote add github git@github.com:BosEriko/obsidian.git
+    git remote add gitlab git@gitlab.com:BosEriko/obsidian.git
+    git remote add bitbucket git@bitbucket.org:BosEriko/obsidian.git
+    git remote set-url --add --push origin git@github.com:BosEriko/obsidian.git
+    git remote set-url --add --push origin git@gitlab.com:BosEriko/obsidian.git
+    git remote set-url --add --push origin git@bitbucket.org:BosEriko/obsidian.git
+  )
 fi
