8da755addf21bf8af04146c914b30ddfc1beb44e
BosEriko <bos@eriko.ph>
2026-02-24 11:54:56 +0800

    Feature: Create Notes Module

---
+++ .files/modules/notes/install.z.sh
@@ -0,0 +1,16 @@
+# =========================================================================== [Installation] ===== #
+
+# Install Obsidian
+if [[ "$OS_TYPE" == "stm" ]]; then
+  NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.obsidian
+  mkdir -p ~/Documents/Notes
+  cd ~/Documents/Notes
+  git init
+  git remote add origin git@github.com:BosEriko/obsidian.git
+  git remote add github git@github.com:BosEriko/obsidian.git
+  git remote add gitlab git@gitlab.com:BosEriko/obsidian.git
+  git remote add bitbucket git@bitbucket.org:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@github.com:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@gitlab.com:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@bitbucket.org:BosEriko/obsidian.git
+fi
