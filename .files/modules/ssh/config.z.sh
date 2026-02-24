702c366074b8c4e85ee2f2133d2c5519b025353b
BosEriko <bos@eriko.ph>
2026-02-24 10:31:03 +0800

    Hotfix: Use proper clipboard on xclip

--- .files/modules/ssh/config.z.sh
+++ .files/modules/ssh/config.z.sh
@@ -1,7 +1,7 @@
 # ========================================================================== [Configuration] ===== #
 
 ssh-copy() {
-  cat ~/.ssh/id_rsa.pub | xclip
+  cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
   echo "SSH Key has been copied to clipboard."
 }
 bos-append programming ssh-key "Copy Main SSH Key to clipboard" "ssh-copy"
