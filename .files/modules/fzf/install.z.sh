b64d3f21c7293829afbcdec9add107337a7526d0
BosEriko <bos@eriko.ph>
2026-02-24 12:39:30 +0800

    Feature: Install FZF

---
+++ .files/modules/fzf/install.z.sh
@@ -0,0 +1,5 @@
+# =========================================================================== [Installation] ===== #
+
+# Install fzf (https://github.com/junegunn/fzf)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
