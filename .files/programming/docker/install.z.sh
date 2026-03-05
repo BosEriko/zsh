a4f9ed45986168d4e29a68fabccf733dda0c905d
BosEriko <bos@eriko.ph>
2026-03-06 07:59:14 +0800

    Feature: Add docker install script

--- .files/programming/docker/install.z.sh
+++ .files/programming/docker/install.z.sh
@@ -5,3 +5,10 @@
 if [[ "$OS_TYPE" == "stm" ]]; then
   nix-env -iA nixpkgs.podman nixpkgs.podman-compose
 fi
+
+if [[ "$OS_TYPE" == "mac" ]]; then
+  brew install docker
+  brew install docker-compose
+  brew install colima
+  brew services start colima
+fi
