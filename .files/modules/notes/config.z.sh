17cdf67c773f0df50c51d015e19754de4700bd80
BosEriko <bos@eriko.ph>
2026-03-06 08:16:21 +0800

    Feature: Remove hardcoded branch

--- .files/modules/notes/config.z.sh
+++ .files/modules/notes/config.z.sh
@@ -4,28 +4,34 @@ NOTES_DIR="$HOME/Documents/Notes"
 HISTFILE="$NOTES_DIR/.zsh_history"
 
 function notes() {
-  if [[ "$1" == "pull" ]]; then
-    (
-      cd "$NOTES_DIR"
+  local cmd="$1"
+
+  (
+    cd "$NOTES_DIR" || exit
+
+    local BRANCH=$(git symbolic-ref --short HEAD)
+
+    case "$cmd" in
+    pull)
       git fetch origin
       rm -rf .zsh_history
-      git pull origin master
-    )
-  elif [[ "$1" == "push" ]]; then
-    (
-      cd "$NOTES_DIR"
+      git pull origin "$BRANCH"
+      ;;
+    push)
       if [[ -n "$(git status --porcelain)" ]]; then
         git add .
         timestamp=$(date "+%Y-%m-%d — %I:%M%p")
         git commit -m ":pencil: $timestamp"
-        git push origin master
+        git push origin "$BRANCH"
       else
         echo "No changes to push."
       fi
-    )
-  else
-    echo "Usage: notes {pull|push}"
-  fi
+      ;;
+    *)
+      echo "Usage: notes {pull|push}"
+      ;;
+    esac
+  )
 }
 
 bos-append notes pull "Pull notes changes" "notes pull"
