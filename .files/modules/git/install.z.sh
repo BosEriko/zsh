6e8630474e429645fe2ae1029f6ceebf24820d22
BosEriko <bos@eriko.ph>
2026-07-18 17:41:00 +0800

    Feature: Add gitmoji

--- .files/modules/git/install.z.sh
+++ .files/modules/git/install.z.sh
@@ -1,6 +1,11 @@
 # =========================================================================== [Installation] ===== #
 
-# Install lolcat and figlet
 if [[ "$OS_TYPE" == "stm" ]]; then
+  # Install lolcat and figlet
   nix-env -iA nixpkgs.figlet nixpkgs.lolcat
+fi
+
+if [[ "$OS_TYPE" == "mac" ]]; then
+  # Install gitmoji
+  brew install gitmoji
 fi
