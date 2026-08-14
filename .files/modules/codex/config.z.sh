0a8766c5662b85fe0ab3f75929ba29abb323a68e
BosEriko <bos@eriko.ph>
2026-08-14 20:46:52 +0800

    ✨ Remove usage of personal config for codex

--- .files/modules/codex/config.z.sh
+++ .files/modules/codex/config.z.sh
@@ -1,7 +1,7 @@
 # ========================================================================== [Configuration] ===== #
 
 start-codex() {
-  tmux new-window -n Codex -c "#{pane_current_path}" "codex -p personal; read -p 'Press Enter to close...'"
+  tmux new-window -n Codex -c "#{pane_current_path}" "codex; read -p 'Press Enter to close...'"
 }
 
 bos-append programming codex "Start codex" "start-codex"
