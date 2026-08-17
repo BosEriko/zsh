2e55d446570a080ce08874a75b8cca23000b7a22
BosEriko <bos@eriko.ph>
2026-08-18 07:33:50 +0800

    ✨ Remove origin before setting origin on agent

--- .files/modules/agent/install.z.sh
+++ .files/modules/agent/install.z.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.config
 git clone https://github.com/BosEriko/AGENTS.md.git ~/.config/agents
 (
   cd ~/.config/agents
+  git remote rm origin
   git remote add origin git@github.com:BosEriko/AGENTS.md.git
   git remote add github git@github.com:BosEriko/AGENTS.md.git
   git remote add gitlab git@gitlab.com:BosEriko/AGENTS.md.git
