3fd468cbc1c7fd7b11e162c574606dcc85ff5426
BosEriko <bos@eriko.ph>
2026-08-18 08:02:33 +0800

    ✨ Update install script to make it more predictable

--- .files/modules/notes/install.z.sh
+++ .files/modules/notes/install.z.sh
@@ -1,18 +1,21 @@
 # =========================================================================== [Installation] ===== #
 
+# Clone repository
+mkdir -p ~/Documents
+git clone https://github.com/BosEriko/obsidian.git ~/Documents/Notes
+(
+  cd ~/Documents/Notes
+  git remote rm origin
+  git remote add origin git@github.com:BosEriko/obsidian.git
+  git remote add github git@github.com:BosEriko/obsidian.git
+  git remote add gitlab git@gitlab.com:BosEriko/obsidian.git
+  git remote add bitbucket git@bitbucket.org:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@github.com:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@gitlab.com:BosEriko/obsidian.git
+  git remote set-url --add --push origin git@bitbucket.org:BosEriko/obsidian.git
+)
+
 # Install Obsidian
 if [[ "$OS_TYPE" == "stm" ]]; then
   NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.obsidian
-  mkdir -p ~/Documents/Notes
-  (
-    cd ~/Documents/Notes
-    git init
-    git remote add origin git@github.com:BosEriko/obsidian.git
-    git remote add github git@github.com:BosEriko/obsidian.git
-    git remote add gitlab git@gitlab.com:BosEriko/obsidian.git
-    git remote add bitbucket git@bitbucket.org:BosEriko/obsidian.git
-    git remote set-url --add --push origin git@github.com:BosEriko/obsidian.git
-    git remote set-url --add --push origin git@gitlab.com:BosEriko/obsidian.git
-    git remote set-url --add --push origin git@bitbucket.org:BosEriko/obsidian.git
-  )
 fi
