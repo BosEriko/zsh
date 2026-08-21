2806965759217dd577cc42246b3475d4f8d5ed17
BosEriko <bos@eriko.ph>
2026-08-22 03:34:25 +0800

    ✨ Bind opencode

--- .tmux/hotkeys.tmux.sh
+++ .tmux/hotkeys.tmux.sh
@@ -42,7 +42,8 @@ bind -Tcopy-mode-vi 'v' send -X begin-selection
 bind -Tcopy-mode-vi 'y' send -X copy-pipe-and-cancel "tmux save-buffer - | reattach-to-user-namespace pbcopy"
 
 # Agent Mode
-bind a new-window -n Codex -c "#{pane_current_path}" "codex resume --last; read -p 'Press Enter to close...'"
+bind-key a new-window -n Codex -c "#{pane_current_path}" "codex resume --last; read -p 'Press Enter to close...'"
+bind-key A new-window -n Opencode -c "#{pane_current_path}" "opencode -c; read -p 'Press Enter to close...'"
 
 # Edit Mode
 bind e new-window -n NeoVim -c "#{pane_current_path}" "nvim .; read -p 'Press Enter to close...'"
