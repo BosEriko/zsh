3afea55fc6813a0d7835cf0498255a8e612bcece
BosEriko <bos@eriko.ph>
2026-09-11 09:14:16 +0800

    ✨ Improve ux on env filler

--- .zsh/env.z.sh
+++ .zsh/env.z.sh
@@ -1,4 +1,13 @@
 # ==================================================================================== [ENV] ===== #
 
-[[ ! -f ~/env.z.sh ]] && return 0
-source ~/env.z.sh
+ENV_FILE=~/env.z.sh
+
+[[ -f "$ENV_FILE" ]] || cp ~/example.env.z.sh "$ENV_FILE"
+grep -q "XXXX" "$ENV_FILE" && vim "$ENV_FILE"
+
+if grep -q "XXXX" "$ENV_FILE"; then
+  echo "env.z.sh still has placeholder values, aborting shell startup"
+  exit 1
+fi
+
+source "$ENV_FILE"
